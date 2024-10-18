1fef3dab9f3149fc5c90edf0ed21808a368564e0
HOUDAHS <houdaslimani16@gmail.com>
2024-10-18 15:25:44 +0100

    dcjj

---
+++ src/Bouton.js
@@ -0,0 +1,12 @@
+
+
+function Bouton({ bouton}) {
+    return (
+      <div className="title-project">
+   <button class="favorite styled" type="button">{ bouton}</button>
+  
+      </div>
+    );
+  }
+  
+  export default Bouton;
---
+++ src/Title.js
@@ -0,0 +1,11 @@
+
+function Title({ title }) {
+  return (
+    <div className="title-project">
+ <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">{title} </h1>
+
+    </div>
+  );
+}
+
+export default Title;
